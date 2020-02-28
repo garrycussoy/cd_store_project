7ab11c74fedb70b3b62b9948fae3bf2e3ede8950
garrycussoy <garryarielcussoy@gmail.com>
2020-02-28 11:33:56 +0700

    Setup tables

---
+++ database/migrations/2020_02_27_140726_create_rent_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rent', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->boolean('returned')->default(False);
+            $table->timestamp('borrowed_time');
+            $table->timestamp('returned_time')->nullable();
+            $table->integer('total_items');
+            $table->bigInteger('total_price');
+
+            $table->foreign('user_id')->references('id')->on('user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rent');
+    }
+}
